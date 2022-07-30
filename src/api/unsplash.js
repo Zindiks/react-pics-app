14a51bd5efce4c2149f61b7ef4fda8d393beedda
Zindiks <103096207+Zindiks@users.noreply.github.com>
2022-07-31 02:15:06 +0300

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
-    headers: {Authorization: 'Client-ID // Your Unsplash API KEY'}
+    headers: {Authorization: 'Client-ID LPxecI5tgPRUosyF8HNY3K75HuNSPTtNixg1q82CkX0'}
 })
 
 
